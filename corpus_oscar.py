f5d4a2e4911a1654c1fe0383e6ff6b80d6e26f0e
coa-la <manhtranduc1111@gmail.com>
2020-06-15 14:58:08 +0300

    update regex

--- corpus_oscar.py
+++ corpus_oscar.py
@@ -30,48 +30,41 @@ def filter_text(text, min_length, max_length):
 
     return True
 
-
-regex_link = re.compile(r'\<a href="(.*?)"\>(.*?)\</a\>')
-
-
 def main(args):
     sent_splitter = VinaSentenceSplitter()
 
     num_processed_docs = 0
     num_doc = 0 
     with open(args.input_file, 'rt') as input_file:
-        with open(str(args.output_file)+str(num_doc), 'w') as output_file:
-            for line in input_file:
-                # replace links
-                text = regex_link.sub(r'\2', line)
+        while(input_file):
+            with open(str(args.output_file)+str(num_doc), 'w') as output_file:
+                for line in input_file:
+                    # replace links
 
-                # normalize text
-                text = unicodedata.normalize('NFC', text)
-                text = word_tokenize(text, format="text")
-                paragraphs = re.split(r'\n\n+', text)[1:]
-                sentences = [preprocess_text(s) for p in paragraphs
-                            for s in sent_splitter(p)]
-                # ignore too short/long sentences
-                sentences = [s for s in sentences
-                            if filter_text(s, args.min_length, args.max_length)]
-                if sentences:
-                    # write document to a file
-                    for s in sentences:
-                        assert not '\n' in s, s
-                        assert s, s
-                        output_file.write(s + '\n')
+                    # normalize text
+                    text = unicodedata.normalize('NFC', text)
+                    text = word_tokenize(text, format="text")
+                    paragraphs = re.split(r'\n\n+', text)[1:]
+                    sentences = [preprocess_text(s) for p in paragraphs
+                                for s in sent_splitter(p)]
+                    # ignore too short/long sentences
+                    sentences = [s for s in sentences
+                                if filter_text(s, args.min_length, args.max_length)]
+                    if sentences:
+                        # write document to a file
+                        for s in sentences:
+                            assert not '\n' in s, s
+                            assert s, s
+                            output_file.write(s + '\n')
 
-                    output_file.write('\n')
-
-                num_processed_docs += 1
-                
-                # logging
-                if num_processed_docs % 10000 == 0:
-                    logger.info('processed: {}'.format(num_processed_docs))
-                    break
-
-            if num_processed_docs % 10000 != 0:
-                logger.info('processed: {}'.format(num_processed_docs))
+                        output_file.write('\n')
+                    num_processed_docs += 1
+                    
+                    # logging
+                    if num_processed_docs % 10000000 == 0:
+                        logger.info('processed: {}'.format(num_processed_docs))
+                        num_doc+=1
+                        break
 
 
 if __name__ == "__main__":

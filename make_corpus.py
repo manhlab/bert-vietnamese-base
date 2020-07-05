b428f5f2bd31a41d4feeed385215ed61488d335b
Manh Lab <47383746+manhlab@users.noreply.github.com>
2020-07-05 11:43:53 +0300

    Update make_corpus.py

--- make_corpus.py
+++ make_corpus.py
@@ -39,7 +39,7 @@ elif lcode == 'th':
 #     print "StanfordSegmenter succesfuly loaded!"
     
 max_corpus_size = args.max_corpus_size
-fname = "{}wiki-20161201-pages-articles-multistream.xml".format(lcode)    
+fname = "{}wiki-20200701-pages-articles-multistream.xml".format(lcode)    
 
 def clean_text(text):
     global lcode
